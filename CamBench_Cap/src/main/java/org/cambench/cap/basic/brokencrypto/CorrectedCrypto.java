3eb9d982d056c19b44338c4bcc1ec5e0f55be30d
nklsd <n.doppelstein@web.de>
2022-09-04 23:13:29 +0200

    minor corrections for basic testcases

--- CamBench_Cap/src/main/java/org/cambench/cap/basic/brokencrypto/CorrectedCrypto.java
+++ CamBench_Cap/src/main/java/org/cambench/cap/basic/brokencrypto/CorrectedCrypto.java
@@ -8,9 +8,8 @@ import java.security.NoSuchAlgorithmException;
 
 public class CorrectedCrypto {
     public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
-        Cipher cipher = Cipher.getInstance("AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         KeyGenerator keyGen = KeyGenerator.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE,keyGen.generateKey());
-        System.out.println(cipher.getAlgorithm() + cipher.getParameters());
     }
 }
