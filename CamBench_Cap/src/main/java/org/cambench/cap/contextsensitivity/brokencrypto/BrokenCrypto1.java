d90cb26768a189f21e579bd491e4a756ff28ec50
nklsd <n.doppelstein@web.de>
2022-09-05 09:15:42 +0200

    Add context-sensitive test cases

---
+++ CamBench_Cap/src/main/java/org/cambench/cap/contextsensitivity/brokencrypto/BrokenCrypto1.java
@@ -0,0 +1,22 @@
+package main.java.org.cambench.cap.contextsensitivity.brokencrypto;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class BrokenCrypto1 {
+    public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        String algo1 = Identity("Blowfish");
+        String algo2 = Identity("AES/CBC/PKCS5Padding");
+
+        Cipher cipher = Cipher.getInstance(algo2);
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE,keyGen.generateKey());
+    }
+
+    public static String Identity(String s){
+        return s;
+    }
+}
